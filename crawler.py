71446402e3144935c439dc3e76aa10dcad702311
sooraj <sooraj@hamon.in>
2022-03-17 11:29:46 +0530

    lyrics fetched from songslyrics website and program working well

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
 
@@ -14,18 +15,50 @@ def get_artists(url):
     return ret
 
 
-def get_songs(song_url):
-    pass
+def get_songs(artist_url):
+    songs = []
+    r = requests.get(artist_url)
+    body = r.content
+    soup = BeautifulSoup(body, features="html.parser")
+    tracklist = soup.find("table", {"class": "tracklist"})
+    links = tracklist.find_all("a")
+    for i in links:
+        songs.append((i.text, i["href"]))
+    return songs
 
 
-def get_artists(artists_url):
-    pass
+def get_lyrics(song_url):
+    r = requests.get(song_url)
+    body = r.content
+    soup = BeautifulSoup(body, features="html.parser")
+    lyrics_div = soup.find("p", {"id": "songLyricsDiv"})
+    lyrics = lyrics_div.text
+    return lyrics
 
 
 def crawl():
-    artists = get_artists("http://www.songlyrics.com/a/")
+    base_dir = "lyrics"
+    try:
+        os.mkdir(base_dir)
+    except Exception:
+        pass
+    artists = get_artists("https://www.songlyrics.com/a/")
     for name, link in artists:
-        print(name, "  :  ", link)
+        print(name, "   :   ", link)
+        name_dir = os.path.join(base_dir, name.replace(" ", "_").lower())
+        try:
+            os.mkdir(name_dir)
+        except Exception:
+            pass
+        songs = get_songs(link)
+        for song, song_link in songs:
+            lyrics = get_lyrics(song_link)
+            song_file = os.path.join(
+                name_dir, song.replace(" ", "_").lower()+".txt")
+            with open(song_file, "w") as f:
+                f.write(lyrics)
+            print(".", end="", flush=True)
+        print("DONE")
 
 
 if __name__ == "__main__":
