33df2d898a0f1a1e34212bda48968c6973509446
sooraj <sooraj@hamon.in>
2022-03-18 11:23:08 +0530

    song name and lyrics downloaded on a single file

--- crawler.py
+++ crawler.py
@@ -37,27 +37,20 @@ def get_lyrics(song_url):
 
 
 def crawl():
-    base_dir = "lyrics"
-    try:
-        os.mkdir(base_dir)
-    except Exception:
-        pass
+
     artists = get_artists("https://www.songlyrics.com/a/")
     for name, link in artists:
         print(name, "   :   ", link)
-        name_dir = os.path.join(base_dir, name.replace(" ", "_").lower())
-        try:
-            os.mkdir(name_dir)
-        except Exception:
-            pass
+
         songs = get_songs(link)
         for song, song_link in songs:
-            lyrics = get_lyrics(song_link)
-            song_file = os.path.join(
-                name_dir, song.replace(" ", "_").lower()+".txt")
-            with open(song_file, "w") as f:
+            with open("lyrics", "a") as f:
+                lyrics = get_lyrics(song_link)
+                f.write("\n\n-----------------\n\n")
+                f.write(song)
+                f.write("\n\n-----------------\n\n")
                 f.write(lyrics)
-            print(".", end="", flush=True)
+
         print("DONE")
 
 
